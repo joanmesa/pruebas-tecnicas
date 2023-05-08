8a511b4561596f9db08fb31353b232c2c09ce421
James Medina <joanmesa021@gmail.com>
2023-05-07 19:05:09 -0600

    Preparando primer ejercicio

---
+++ ejercicio001.js
@@ -0,0 +1,14 @@
+/*
+Enunciado ejercicio 1:
+Dado un array de superhéroes de Marvel, az un programa que pueda mostrar la información de un superheroe.
+
+Y añande una capacidad de buscar la información de varios superhéroes a la vez.
+
+Ejemplo:
+mostrarInformacionSuper('Iron Man');
+
+Salida:
+Nombre real: Tony Satark
+Poderes: Tecnología avanzada, movilidad aerea
+Equipo: Los venadores
+*/
