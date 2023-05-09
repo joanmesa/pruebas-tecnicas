245a85b2d0810d7c10a19ce8ae9f7cbc744e6806
James Medina <joanmesa021@gmail.com>
2023-05-08 21:15:23 -0600

    Añadiendo funcion mostrar superheroe

--- ejercicio001.js
+++ ejercicio001.js
@@ -1,14 +1,70 @@
 /*
-Enunciado ejercicio 1:
-Dado un array de superhéroes de Marvel, az un programa que pueda mostrar la información de un superheroe.
 
-Y añande una capacidad de buscar la información de varios superhéroes a la vez.
+Enunciado Ejercicio 1:
+Dado un array o un objeto de superhéroes de Marvel, haz un programa que pueda
+mostrar la información de un superheroe.
+
+Y añade una capacidad de buscar la información de varios superhéroes a la vez.
 
 Ejemplo:
-mostrarInformacionSuper('Iron Man');
+mostrarInformacionSuperheroe('Iron Man');
 
 Salida:
-Nombre real: Tony Satark
-Poderes: Tecnología avanzada, movilidad aerea
-Equipo: Los venadores
+Nombre real: Tony Stark
+Poderes: Tecnología avanzada, Movilidad aérea
+Equipo: Los vengadores
+
+Ejemplo 2: 
+mostrarInformacionSuperheroes([array de nombres]]);
+
+Salida:
+Muestra la información de todos los superheroes
+
 */
+
+// Crear array de objetos de superheroes marvel
+const infoSuperheroes = {
+    'Iron Man': {
+        nombreReal: 'Tony Stark',
+        poderes: ['Tecnologia avanzada', 'Movilidad aérea'],
+        equipo: 'Los vengadores'
+    },
+    'Capitán América': {
+        nombreReal: 'Steve Rogers',
+        poderes: ['Fuerza sobrehumana', 'Agilidad y reflejos sobresalientes'],
+        equipo: 'Los vengadores'
+    },
+    'Thor': {
+        nombreReal: 'Thor Odinson',
+        poderes: ['Mjolnir', 'Viento y trueno'],
+        equipo: 'Los vengadores'
+    },
+    'Spider-Man': {
+        nombreReal: 'Peter Parker',
+        poderes: ['Balanceo', 'Telarañas pegajosas', 'Sentido aracnido'],
+        equipo: 'Los vengadores'
+    },
+    'Hulk': {
+        nombreReal: 'Bruce Banner',
+        poderes: ['Fuerza sobrehumana', 'Invulnerabilidad'],
+        equipo: 'Los vengadores'
+    }
+};
+
+
+const showSuperheroInfo = (nameSuperheroe) => {
+    let descriptionSuperheroe = '';
+    for (const [key, value] of Object.entries(infoSuperheroes)) {
+        if (key === nameSuperheroe) {
+            const { nombreReal, poderes, equipo } = value;
+            descriptionSuperheroe += `Nombre real : ${nombreReal}\n`;
+            descriptionSuperheroe += `Poderes : ${poderes}\n`;
+            descriptionSuperheroe += `Equipo : ${equipo}`;
+        }
+    }
+    return descriptionSuperheroe;
+}
+
+console.log(showSuperheroInfo('Iron Man'));
+
+
